80ac9e064258dc0e61d2fbe254352bfac8cf53c3
Mimi Chenyao <mimichenyao@gmail.com>
2017-12-19 00:47:31 -0500

    Initial commit

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Mimi Chenyao on 12/17/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startNewRound()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let difference = abs(currentValue - targetValue)
+        
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+    }
+}
+
